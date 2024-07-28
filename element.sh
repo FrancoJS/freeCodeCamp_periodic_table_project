cfea9b7fe7ea94fdebd567d34c8c565cd87e51b7
FrancoJS <jean.jara.fs@gmail.com>
2024-07-28 19:26:24 +0000

    feat: agregue consulta para obtener todas las columnas necesarias

--- element.sh
+++ element.sh
@@ -2,9 +2,16 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 MAIN(){
-  echo "Please provide an element as an argument."
-  read ELEMENT
-
+  if [[ ! $1 ]]
+  then
+    echo "Please provide an element as an argument."
+  fi
+  
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+  IFS='|' read -r ATOMIC_NUMBER SYMBOL NAME TYPE ATOMIC_MASS MELTING_P_C BOILING_P_C < <($PSQL "select elements.atomic_number, symbol,name, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties on elements.atomic_number = properties.atomic_number inner join types on types.type_id = properties.type_id where elements.atomic_number = $1")
+  echo $ATOMIC_NUMBER $SYMBOL $NAME $TYPE $ATOMIC_MASS $MELTING_P_C $BOILING_P_C
+  fi
 }
 
-MAIN
+MAIN $1
